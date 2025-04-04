2d39f846f542c108cd107737a75bd732cbe52d8d
Laila Suter <e227401@edu.sbl.ch>
2025-04-04 12:01:43 +0000

    Änderung Level 2

--- backlog.md
+++ backlog.md
@@ -3,8 +3,8 @@
 - [x] README.md erstellen
 - [ ] Contributors ausfüllen in `package.json` (gleiches Format wie author)
 - [ ] Dokumentation vollständig durchlesen
-- [ ] Name des Spiels ändern
-- [ ] Spielidee auf der Webseite beschreiben (kann später noch erweitert werden)
+- [x] Name des Spiels ändern
+- [x] Spielidee auf der Webseite beschreiben (kann später noch erweitert werden)
 - [ ] Eigene Spritesheets erstellen
 - [ ] Präsentation für das Endprodukt in (`pres.html`) anpassen
 - [ ] Komponenten für Items hinzufügen.
--- src/game/scenes/levels/level-02.js
+++ src/game/scenes/levels/level-02.js
@@ -19,4 +19,37 @@ export default class Level02 extends Base2DScene {
   create() {
     super.create("map-level-02")
   }
+  pickUp(actor, item) {
+    super.pickUp(actor, item)
+
+    // TODO: Hier wird die Logik für Kollisionen von Spielobjekten geändert. Das
+    // ist pro Level anders. Wenn eine Logik für alle Levels gelten soll, dann
+    // muss dies in `Base2DScene` angepasst werden.
+    if (item instanceof Flower) {
+      // Das Objekt ist von der Klasse `Flower`
+      this.player.addKey("level-02")
+      this.player.increaseSpeed(100)
+      this.player.heal(item.props.restoreHp || 0)
+    } else if (item instanceof Mushroom) {
+      // Das Objekt ist von der Klasse `Mushroom`
+      this.player.decreaseSpeed(100)
+      this.player.damage(item.props.damageHp || 0)
+
+      // TODO: Aktivieren Sie das hier, wenn ein Effekt über eine gewisse Zeit
+      // passieren soll.
+      // Hier wird der Spieler halb so gross, und mit jedem Frame wird er wieder
+      // normaler. Nach 6 Sekunden erreicht er seine normale Grösse.
+      this.tweens.addCounter({
+        from: 0.5,
+        to: 1,
+        ease: "Linear",
+        duration: 6000,
+        repeat: 0,
+        onUpdate: (tween) => {
+          const val = tween.getValue()
+          this.player.setScale(val)
+        },
+      })
+    }
+  }
 }
